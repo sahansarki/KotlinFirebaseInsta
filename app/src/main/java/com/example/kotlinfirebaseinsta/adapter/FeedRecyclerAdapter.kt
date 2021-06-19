463556dc2f7f2f460b3c5351e519136fcfac9a98
sahan <sahancansarki@gmail.com>
2021-06-20 02:14:09 +0300

    FeedRecyclerAdapter Güncellendi

--- app/src/main/java/com/example/kotlinfirebaseinsta/adapter/FeedRecyclerAdapter.kt
+++ app/src/main/java/com/example/kotlinfirebaseinsta/adapter/FeedRecyclerAdapter.kt
@@ -41,23 +41,28 @@ class FeedRecyclerAdapter(var post: ArrayList<Post>?, var feedViewModel: FeedAct
 
     override fun onBindViewHolder(holder: PostHolder, position: Int) {
 
-
-
-        if(post!![position].users.contains(FirebaseLogic.auth.currentUser!!.email)) {
-            holder.itemView.recyclerLikeButton.tag = "like"
-            holder.itemView.recyclerLikeButton.setBackgroundResource(R.drawable.ig_like)
-
-        } else {
-            holder.itemView.recyclerLikeButton.setBackgroundResource(R.drawable.ig_unlike)
-            holder.itemView.recyclerLikeButton.tag = "dislike"
-        }
-
-        holder.itemView.recyclerCommentText?.text =
-            if (post != null) post!![position].comment else ""
-
-        holder.itemView.recyclerLikeNumber.text = if(post != null) post!![position].likeNumber.toString() else "0"
+        val recyclerLikeButton = holder.itemView.recyclerLikeButton
+        val recyclerCommentText = holder.itemView.recyclerCommentText
+        val recyclerLikeNumber = holder.itemView.recyclerLikeNumber
+        val recyclerCommentName = holder.itemView.recyclerCommentName
+        val recyclerEmailText = holder.itemView.recyclerEmailText
+        val recyclerImageView = holder.itemView.recyclerImageView
+        val recyclerUserPhoto = holder.itemView.recyclerUserPhoto
 
         if (post != null) {
+            if (post!![position].users.contains(FirebaseLogic.auth.currentUser!!.email)) {
+                recyclerLikeButton.tag = "like"
+                recyclerLikeButton.setBackgroundResource(R.drawable.ig_like)
+
+            } else {
+                recyclerLikeButton.setBackgroundResource(R.drawable.ig_unlike)
+                recyclerLikeButton.tag = "dislike"
+            }
+
+            recyclerCommentText?.text = post!![position].comment
+
+            recyclerLikeNumber.text = post!![position].likeNumber.toString()
+
 
 
             FirebaseLogic.db.collection("Users").addSnapshotListener { snapshot, exception ->
@@ -67,10 +72,10 @@ class FeedRecyclerAdapter(var post: ArrayList<Post>?, var feedViewModel: FeedAct
                         for (document in documents) {
                             if (document.get("email") as String == post!![position].userEmail) {
                                 Picasso.get().load(document.get("photoUrl") as String)
-                                    .into(holder.itemView.recyclerUserPhoto)
-                                holder.itemView.recyclerCommentName?.text =
+                                    .into(recyclerUserPhoto)
+                                recyclerCommentName?.text =
                                     document.get("username") as String
-                                holder.itemView.recyclerEmailText?.text =
+                                recyclerEmailText?.text =
                                     document.get("email") as String
                             }
                         }
@@ -79,45 +84,52 @@ class FeedRecyclerAdapter(var post: ArrayList<Post>?, var feedViewModel: FeedAct
             }
 
 
-            Picasso.get().load(post!![position].downloadUrl).into(holder.itemView.recyclerImageView)
+            Picasso.get().load(post!![position].downloadUrl).into(recyclerImageView)
+
+
+            recyclerEmailText.setOnClickListener {
+                feedViewModel?.userView?.value = it
+
+            }
+
+            recyclerLikeButton.setOnClickListener {
+                var like = post!![position].likeNumber
+                if (recyclerUserPhoto.tag == "dislike") {
+                    like++
+                    recyclerUserPhoto.setBackgroundResource(R.drawable.ig_like)
+                    recyclerUserPhoto.tag = "like"
+                    post!![position].likeNumber++
+
+                    recyclerLikeNumber.text = "$like"
+                    FirebaseLogic.updateLikeNumber(
+                        post!![position],
+                        recyclerEmailText.text.toString(),
+                        true
+                    )
+
+
+                } else {
+                    like--
+                    recyclerUserPhoto.setBackgroundResource(R.drawable.ig_unlike)
+                    recyclerUserPhoto.tag = "dislike"
+                    post!![position].likeNumber--
+                    recyclerLikeNumber.text = "$like"
+                    FirebaseLogic.updateLikeNumber(
+                        post!![position],
+                        recyclerEmailText.text.toString(),
+                        false
+                    )
+
+                }
+                notifyDataSetChanged()
+            }
+
+
         } else {
             Picasso.get()
                 .load("https://firebasestorage.googleapis.com/v0/b/kotlinfirebaseinsta-43eca.appspot.com/o/images%2Fdefec551-edb3-4571-a5c0-f3ee5820a4c4.jpg?alt=media&token=68d50e45-b295-4a8e-bef7-6347b4c5fdcb")
-                .into(holder.itemView.recyclerImageView)
+                .into(recyclerImageView)
         }
-
-        holder.itemView.recyclerEmailText.setOnClickListener {
-            feedViewModel?.userView?.value = it
-
-        }
-
-        holder.itemView.recyclerLikeButton.setOnClickListener {
-            var like = post!![position].likeNumber
-            if(it.tag == "dislike") {
-                like++
-                it.setBackgroundResource(R.drawable.ig_like)
-                it.tag = "like"
-                post!![position].likeNumber++
-
-                holder.itemView.recyclerLikeNumber.text = "$like"
-                FirebaseLogic.updateLikeNumber(post!![position], it.rootView.recyclerEmailText.text.toString(),true)
-
-
-
-            } else {
-                like--
-                it.setBackgroundResource(R.drawable.ig_unlike)
-                it.tag = "dislike"
-                post!![position].likeNumber--
-                holder.itemView.recyclerLikeNumber.text = "$like"
-                FirebaseLogic.updateLikeNumber(post!![position], it.rootView.recyclerEmailText.text.toString(),false)
-
-            }
-            notifyDataSetChanged()
-
-        }
-
-
 
 
     }
